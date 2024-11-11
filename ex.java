de6a56c5df723595d130c7aee3db8c7b845840da
gayathri39103 <132190482+gayathri39103@users.noreply.github.com>
2024-11-11 16:17:08 +0530

    Update ex.java
    removing the input taking code lines

--- ex.java
+++ ex.java
@@ -4,9 +4,9 @@ class tab
        public static void main(String args[])
        {
              int n,i;
-             Scanner s=new Scanner(System.in);
-             System.out.println("enter n");
-             n=s.nextInt();
+             //Scanner s=new Scanner(System.in);
+             //System.out.println("enter n");
+             n=4;
              for(i=1;i<=10;i++)
              {
                  System.out.println(n+"*"+i+"="+n*i);
